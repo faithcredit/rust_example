2a8064f103dfad76ee006cae4247fa209cd4fcfd
faithcredit <honest.dedicate@gmail.com>
2023-07-13 15:40:51 +0900

    Fetching top products

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,142 @@
 // -------------------------------------------
-// 			Items in Range
+// 			Fetching Top Products
 //           	- Description
-//           	    - Give a vector of product prices, search for items in a given price range
-//
+//           	    -
 //           	- Tools
-//           	    - Binary Search Tree, Box pointer
+//           	    - Linklist + Iterators
 // -------------------------------------------
 
-struct BinarySearchTree {
-    root: Node,
+#[derive(Debug)]
+
+struct Linklist<T: std::fmt::Debug> {
+    head: pointer<T>,
 }
 
-#[derive(Clone)]
-struct Node {
-    val: i32,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+#[derive(Debug)]
+
+struct Node<T> {
+    element: T,
+    next: pointer<T>,
 }
 
-impl Node {
-    fn new(value: i32) -> Self {
-        Node {
-            val: value,
-            left: None,
-            right: None,
-        }
+type pointer<T> = Option<Box<Node<T>>>;
+
+impl<T: std::fmt::Debug> Linklist<T> {
+    fn create_empty_list() -> Linklist<T> {
+        Linklist { head: None }
     }
 
-    fn insert(&mut self, value: i32) {
-        if value > self.val {
-            match self.right {
-                None => {
-                    self.right = Some(Box::new(Node {
-                        val: value,
-                        left: None,
-                        right: None,
-                    }))
-                }
-                Some(ref mut node) => node.insert(value),
+    fn add(&mut self, element: T) {
+        let previous_head = self.head.take();
+        let new_head = Box::new(Node {
+            element: element,
+            next: previous_head,
+        });
+        self.head = Some(new_head);
+    }
+
+    fn reverse(&mut self) {
+        if self.head.is_none() || self.head.as_ref().unwrap().next.is_none() {
+            return;
+        }
+
+        let mut prev = None;
+        let mut current_node = self.head.take();
+        while current_node.is_some() {
+            let next = current_node.as_mut().unwrap().next.take();
+            current_node.as_mut().unwrap().next = prev.take();
+            prev = current_node.take();
+            current_node = next;
+        }
+
+        self.head = prev.take();
+    }
+
+    fn remove(&mut self) -> Option<T> {
+        let previous_head = self.head.take();
+        match previous_head {
+            Some(old_head) => {
+                self.head = old_head.next;
+                Some(old_head.element)
             }
-        } else {
-            match self.left {
-                None => {
-                    self.left = Some(Box::new(Node {
-                        val: value,
-                        left: None,
-                        right: None,
-                    }))
+            None => None,
+        }
+    }
+    fn peek(&self) -> Option<&T> {
+        match &self.head {
+            Some(H) => Some(&H.element),
+            None => None,
+        }
+    }
+    fn printing(&self) {
+        let mut list_traversal = &self.head;
+        println!();
+
+        while true {
+            match list_traversal {
+                Some(Node) => {
+                    print!(" {:?} ", Node.element);
+                    list_traversal = &list_traversal.as_ref().unwrap().next; // It may think that you may try to change the underlying data
                 }
-                Some(ref mut node) => node.insert(value),
+                None => break,
             }
         }
     }
 }
 
-fn traversal(node: Option<Box<Node>>, low: i32, high: i32, mut output: &mut Vec<i32>) {
-    if !node.is_none() {
-        if node.as_ref().unwrap().val >= low && node.as_ref().unwrap().val <= high {
-            output.push(node.as_ref().unwrap().val);
+fn sorting_lists(vec_list: &mut Vec<Linklist<i32>>) -> Linklist<i32> {
+    let mut sortted_list: Linklist<i32> = Linklist::create_empty_list();
+
+    let mut values: Vec<i32> = Vec::new();
+
+    while true {
+        let values = vec_list
+            .into_iter()
+            .map(|x| x.head.as_ref().unwrap().element)
+            .collect::<Vec<i32>>();
+
+
+        let min_val = *values.iter().min().unwrap();
+        let min_index = values.iter().position(|x| *x == min_val).unwrap();
+        sortted_list.add(min_val);
+
+
+        vec_list[min_index].remove();
+
+        if vec_list[min_index].head.is_none() {
+            
+            vec_list.remove(min_index);
+            
         }
-        if node.as_ref().unwrap().val >= low {
-            traversal(node.as_ref().unwrap().left.clone(), low, high, &mut output);
-        }
-        if node.as_ref().unwrap().val <= high {
-            traversal(node.as_ref().unwrap().right.clone(), low, high, &mut output);
+
+        if vec_list.len() == 0 {
+            break;
         }
     }
-}
-
-fn productsInRange(root: Node, low: i32, high: i32) -> Vec<i32> {
-    let mut output: Vec<i32> = Vec::new();
-    traversal(Some(Box::new(root)), low, high, &mut output);
-    output
+    sortted_list
 }
 
 fn main() {
-    // Driver code
-    let product_prices = vec![9, 6, 14, 20, 1, 30, 8, 17, 5, 15,45,654,54,54,54,5,5,44,5,45,454,5,54,5,5,5,41,45,4645,654,7,89,465,5,45,5,5,5,111,13,321,];
-    let mut bst = BinarySearchTree {
-        root: Node::new(product_prices[0]),
-    };
+    // an empty list
+    let mut list1 = Linklist::create_empty_list();
+    list1.add(45);
+    list1.add(40);
+    list1.add(35);
+    list1.add(23);
+    list1.add(11);
 
-    
+    let mut list2 = Linklist::create_empty_list();
+    list2.add(60);
+    list2.add(44);
 
-    for i in 1..product_prices.len() {
-        bst.root.insert(product_prices[i]);
-    }
+    let mut list3 = Linklist::create_empty_list();
+    list3.add(85);
+    list3.add(20);
+    list3.add(15);
 
-    let result = productsInRange(bst.root, 3, 500);
-    println!("{:?}", result);
+    let mut result = sorting_lists(&mut vec![list1, list2, list3]);
+    result.printing();
+
+    result.reverse();
+    result.printing();
 }
